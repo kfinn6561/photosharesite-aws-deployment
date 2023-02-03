0fe709485af28bdff8158c75f90ff5b8dfb751fc
Kieran Finn <kieran.finn@skyscanner.net>
2023-02-03 17:16:28 +0000

    modified example instance to be more general

--- main.tf
+++ main.tf
@@ -1,8 +1,24 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "HelloWorld"
   }
 }
