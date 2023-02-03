7060d7e5cf3329abd824551500a72429a18a60c0
Kieran Finn <kieran.finn@skyscanner.net>
2023-02-03 17:03:32 +0000

    added terraform required providers

--- providers.tf
+++ providers.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
 # Declaring AWS Provider named 'aws'
 provider "aws" {
   shared_config_files      = ["/Users/kieranfinn/.aws/config"]
