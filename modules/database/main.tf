e39effe954e8890e9a455b9c1d5c9db3844d3437
Kieran Finn <kieran.finn@skyscanner.net>
2023-02-10 17:48:25 +0000

    added ssh key to ec2 instance

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -3,18 +3,23 @@ resource "random_password" "admin-pwd" {
   special = false
 }
 
+module "ssh_key" {
+  source = "../common/ssh_key"
+  key_name = "pss-database-key"
+}
+
+resource "aws_key_pair" "aws-ssh-key" {
+  key_name = module.ssh_key.key_name
+  public_key = module.ssh_key.public_key
+}
+
 resource "aws_instance" "db" {
   ami           = "ami-85a2ade3" #MySQL 5.7
   instance_type = "t2.micro"
+  key_name = aws_key_pair.aws-ssh-key.key_name
+
   user_data     = templatefile("${path.module}/startup.sh", {})
 
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ec2-user"
-    private_key = file("/home/rahul/Jhooq/keys/aws/aws_key")
-    timeout     = "4m"
-  }
 
   tags = {
     Name = "Database"
---
+++ modules/database/output.tf
@@ -0,0 +1,3 @@
+output "ssh-private-key" {
+  value = module.ssh_key.private_key
+}
