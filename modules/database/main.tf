15ddb702e8e58dc0eeb095dc68ff6c21e8fd759b
Kieran Finn <kieran.finn@skyscanner.net>
2023-02-10 12:33:06 +0000

    moved AMIs to their own repo and made DB use MYSQL

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,21 +1,6 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-85a2ade3" #MySQL 5.7
   instance_type = "t2.micro"
 
   tags = {
