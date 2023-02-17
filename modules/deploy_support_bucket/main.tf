3fc9055a7ffe2125ea186def0b12866a3f18c762
Kieran Finn <kieran.finn@skyscanner.net>
2023-02-17 18:33:40 +0000

    created policy and role

--- modules/deploy_support_bucket/main.tf
+++ modules/deploy_support_bucket/main.tf
@@ -1,10 +1,12 @@
+data "aws_canonical_user_id" "current" {}
+
 resource "random_string" "bucket_ending" {
   length    = 4
   min_lower = 4
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "kieran-finn-pss-deployment-support-${random_string.bucket_ending.result}"
+  bucket        = "kieran-finn-pss-deployment-support-${random_string.bucket_ending.result}"
   force_destroy = true
 
   tags = {
@@ -12,10 +14,68 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
+resource "aws_iam_role" "bucket-reader-role" {
+  name = "bucket_reader_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        SID = ""
+      },
+    ]
+  })
 }
+
+
+resource "aws_iam_policy" "bucket-reader-policy" {
+  name        = "bucket_reader_policy"
+  description = "policy allowing reading of the deploy support bucket"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:Get*",
+          "s3:List*"
+        ]
+        Resource = [
+          aws_s3_bucket.bucket.arn
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_s3_bucket_acl" "pss-deploy-support-bucket-acl" {
+  bucket = aws_s3_bucket.bucket.id
+  access_control_policy {
+    grant {
+      grantee {
+        id   = aws_iam_role.bucket-reader-role.id
+        type = "CanonicalUser"
+      }
+      permission = "READ"
+    }
+
+    owner {
+      id = data.aws_canonical_user_id.current.id
+    }
+  }
+}
+
+
 
 resource "aws_s3_object" "db-procedures" {
   for_each = fileset("${var.database-directory}/procedures", "**")
