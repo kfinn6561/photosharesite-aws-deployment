fe2cdd3aba97d0dae9e5723ebe656928a6e90160
Kieran Finn <kieran.finn@skyscanner.net>
2023-02-17 16:38:53 +0000

    changed ssh key to RSA

--- modules/common/ssh_key/output.tf
+++ modules/common/ssh_key/output.tf
@@ -3,10 +3,10 @@ output "key_name" {
 }
 
 output "private_key" {
-  value     = tls_private_key.ed25519.private_key_pem
+  value     = tls_private_key.RSA.private_key_pem
   sensitive = "true"
 }
 
 output "public_key" {
-  value = tls_private_key.ed25519.public_key_openssh
+  value = tls_private_key.RSA.public_key_openssh
 }
