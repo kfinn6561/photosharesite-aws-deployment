2133366d97939eccfc08a23e5d96366ef72eeec4
Kieran Finn <kieran.finn@hotmail.com>
2023-03-01 21:11:06 +0000

    added pathexpand to the root_directory variable

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "root_directory" {
   description = "root directory"
   type        = string
-  default     = "/Users/kieranfinn/photo_share_site"
+  default     = pathexpand("~/photo_share_site")
 }
 
 variable "db_dir_name" {
