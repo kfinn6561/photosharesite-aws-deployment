f60391815a1dd94c24321a951b141748dcbb9301
Kieran Finn <kieran.finn@skyscanner.net>
2023-03-03 16:23:12 +0000

    added VPCID to bucket module

--- main.tf
+++ main.tf
@@ -1,14 +1,20 @@
+module "network" {
+  source = "./modules/network"
+}
+
+
 module "deploy-support-bucket" {
   source = "./modules/deploy_support_bucket"
 
   database-directory = pathexpand("${var.root_directory}/${var.db_dir_name}")
+  VPC-ID             = module.network.vpc-id
 }
 
 
 module "database" {
   source = "./modules/database"
 
-  database-name              = "photo-share-site"
-  bucket-reader-policy-arn   = module.deploy-support-bucket.bucket-reader-policy-arn
+  database-name            = "photo-share-site"
+  bucket-reader-policy-arn = module.deploy-support-bucket.bucket-reader-policy-arn
   deploy-support-bucket-id = module.deploy-support-bucket.bucket-id
 }
--- modules/deploy_support_bucket/input.tf
+++ modules/deploy_support_bucket/input.tf
@@ -2,3 +2,8 @@ variable "database-directory" {
   description = "directory of the database files to upload"
   type        = string
 }
+
+variable "VPC-ID" {
+  description = "ID of the VPC"
+  type        = string
+}
