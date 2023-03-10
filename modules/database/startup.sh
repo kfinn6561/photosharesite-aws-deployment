bfaa5e444f5024ae3c0096b57bfc0eba22021185
Kieran Finn <kieran.finn@skyscanner.net>
2023-03-10 16:11:20 +0000

    install mysql-server from scratch

--- modules/database/startup.sh
+++ modules/database/startup.sh
@@ -2,11 +2,14 @@
 # shellcheck disable=SC2164
 cd /home/ec2-user
 
+yum -y update
+
 # copy all sql files from S3
 aws s3 sync s3://"${bucket-name}"/procedures/ ./procedures
 aws s3 sync s3://"${bucket-name}"/tables/ ./tables
 
 # install mysql
+yum -y install mysql-server
 yum -y install mysql
 
 # create the database
