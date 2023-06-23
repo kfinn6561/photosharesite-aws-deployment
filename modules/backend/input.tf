9ed6f6602c2219a6e94d619e77abbccb02be4ba6
Kieran Finn <kieran.finn@skyscanner.net>
2023-06-23 16:06:14 +0100

    started adding backend iam roles

---
+++ modules/backend/input.tf
@@ -0,0 +1,14 @@
+variable "bucket-reader-policy-arn" {
+  description = "arn of the bucket reader policy"
+  type        = string
+}
+
+variable "bucket-writer-policy-arn" {
+  description = "arn of the bucket writer policy"
+  type        = string
+}
+
+variable "media-files-bucket-id" {
+  description = "id of the media files bucket"
+  type        = string
+}
---
+++ modules/backend/main.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_role" "backend-role" {
+  name = "backend_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "bucket-reader-attach" {
+  name       = "bucket-reader-attachment"
+  roles      = [aws_iam_role.backend-role.name]
+  policy_arn = var.bucket-reader-policy-arn
+}
+
+resource "aws_iam_policy_attachment" "bucket-writer-attach" {
+  name       = "bucket-writer-attachment"
+  roles      = [aws_iam_role.backend-role.name]
+  policy_arn = var.bucket-writer-policy-arn
+}
+
+
+
+resource "aws_iam_access_key" "backend-user-access-key" {
+  user = "your_iam_username"
+}
