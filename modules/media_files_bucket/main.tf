df7025a7bcf205fc04b0ae464f2b43715617f7f8
Kieran Finn <kieran.finn@skyscanner.net>
2023-06-23 15:23:44 +0100

    finished creating media_files_bucket module

---
+++ modules/media_files_bucket/main.tf
@@ -0,0 +1,60 @@
+resource "random_string" "bucket_ending" {
+  length    = 4
+  min_lower = 4
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket        = "kieran-finn-pss-media-files-${random_string.bucket_ending.result}"
+  force_destroy = true
+
+  tags = {
+    Name = "pss-media-files"
+  }
+}
+
+resource "aws_iam_policy" "bucket-reader-policy" {
+  name        = "bucket_reader_policy"
+  description = "policy allowing reading of the media files bucket"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:Get*",
+          "s3:List*"
+        ]
+        Resource = [
+          aws_s3_bucket.bucket.arn,
+          "${aws_s3_bucket.bucket.arn}/*"
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "bucket-writer-policy" {
+  name        = "bucket_reader_policy"
+  description = "policy allowing reading of the media files bucket"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:Put*",
+          "s3:Delete*"
+        ]
+        Resource = [
+          aws_s3_bucket.bucket.arn
+        ]
+      },
+    ]
+  })
+}
